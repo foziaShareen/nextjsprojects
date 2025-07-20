92ad1b7ff134b782c0c862a828ffbd478c5762ec
foziaShareen <foziashareen1@gmail.com>
2025-07-20 16:39:35 +0500

    status code and status text

--- app/todo/[id]/route.js
+++ app/todo/[id]/route.js
@@ -4,6 +4,8 @@ export async function GET(_, { params }) {
     const todo = todos.find((todo) => todo.id == id);
 
     return new Response(JSON.stringify(todo), {
-        headers: { "Content-Type": "application/json" }
+        headers: { "Content-Type": "application/json" },
+        status:234, // Example status code, can be changed as needed
+        statusText: "Custom status text"
     });
 }
