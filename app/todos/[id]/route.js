088219f9af0950ae178663d821c137549934312d
foziaShareen <foziashareen1@gmail.com>
2025-07-22 18:27:32 +0500

    edit functionality

--- app/todos/[id]/route.js
+++ app/todos/[id]/route.js
@@ -1,8 +1,10 @@
-import todos from "../../../todos";
+import { writeFile } from "node:fs/promises";
+import todos from "../../../todos.json";
 
 export async function GET(_, { params }) {
-  const { id } = await params;
-  const todo = todos.find((todo) => id === todo.id);
+  const { id } = params;
+  console.log("GET todo with id:", id);
+  const todo = todos.find((todo) => String(id) === String(todo.id));
 
   if (!todo) {
     return Response.json(
@@ -13,4 +15,35 @@ export async function GET(_, { params }) {
     );
   }
   return Response.json(todo);
+}
+
+export async function PUT(request, { params }) {
+  const editTodoData = await request.json();
+  const { id } = params;
+  const todoIndex = todos.findIndex((todo) => String(id) === String(todo.id));
+  const todo = todos[todoIndex];
+
+  if (!todo) {
+    return Response.json(
+      { error: "Todo not found" },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  if (editTodoData.id) {
+    return Response.json(
+      { error: "Changing ID is not allow." },
+      {
+        status: 403,
+      }
+    );
+  }
+
+  const editedTodo = { ...todo, ...editTodoData };
+  todos[todoIndex] = editedTodo;
+
+  await writeFile("d:/projects/todos-nextjs/todos/todos.json", JSON.stringify(todos, null, 2));
+  return Response.json(editedTodo);
 }
